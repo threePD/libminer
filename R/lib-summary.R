378970d1505df58308198b0326d62a380617dbcb
Patrick Donohue <j.patrick.donohue@gmail.com>
2023-09-17 15:31:07 -0400

    added tests for lib_summary

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -14,6 +14,6 @@ lib_summary <- function() {
   pkgs <- utils::installed.packages()
   pkg_tbl <- table(pkgs[, "LibPath"])
   pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
-  names(pkg_df) <- c("Llbrary", "n_packages")
+  names(pkg_df) <- c("Library", "n_packages")
   return(pkg_df)
 }
