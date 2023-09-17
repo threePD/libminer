391120bb2e37be114b587ffaa55bd18d8a8e7408
Patrick Donohue <j.patrick.donohue@gmail.com>
2023-09-17 17:08:26 -0400

    Updated test for lib_summary to first check ncols then check names

--- tests/testthat/test-lib-summary.R
+++ tests/testthat/test-lib-summary.R
@@ -13,7 +13,7 @@ test_that("lib_summary fails appropriately", {
 
 test_that("sizes argument works", {
   res <- lib_summary(sizes = TRUE)
+  expect_equal(ncol(res), 3)
   expect_equal(names(res), c("Library", "n_packages", "lib_size"))
   expect_type(res[["lib_size"]], "double")
-  expect_equal(ncol(res), 3)
 })
