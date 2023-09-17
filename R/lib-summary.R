40d9ab290ea96ab33108e1e66539e6b71e775b24
Patrick Donohue <j.patrick.donohue@gmail.com>
2023-09-17 16:55:38 -0400

    Added sizes parameter to lib_summary with tests

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -10,10 +10,22 @@
 #'
 #' @examples
 #' lib_summary()
-lib_summary <- function() {
+lib_summary <- function(sizes = FALSE) {
   pkgs <- utils::installed.packages()
   pkg_tbl <- table(pkgs[, "LibPath"])
   pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
   names(pkg_df) <- c("Library", "n_packages")
+
+  if (sizes) {
+    pkg_df[["lib_size"]] <- sapply(
+      pkg_df[["Library"]],
+      function(x) {
+        sum(fs::file_size(fs::dir_ls(x, recurse = TRUE)))
+      }
+    )
+  }
+
   return(pkg_df)
 }
+
+
