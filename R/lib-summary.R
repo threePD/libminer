6eba909192482218a844222655ec48bfbab15892
Patrick Donohue <j.patrick.donohue@gmail.com>
2023-09-17 14:47:17 -0400

    Updated lib_summary Roxygen2 with explicit title and description

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -1,6 +1,7 @@
-#' R Library Summary
+#' @title R Library Summary
 #'
-#' Provides a brief summary of the package libraries on your machine
+#' @description Provides a brief summary of the package libraries on
+#' your machine
 #'
 #' @return A `data.frame` containing the count of packages in each of
 #' the user's libraries.
@@ -14,6 +15,5 @@ lib_summary <- function() {
   pkg_tbl <- table(pkgs[, "LibPath"])
   pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
   names(pkg_df) <- c("Llbrary", "n_packages")
-
   return(pkg_df)
 }
